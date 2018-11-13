7ed793f3cf99269f258b6a32ad82dc3276808b20
lijc <lijc0208@163.com>
2018-11-13 14:58:10 +0800

    启动类扫描不到dao层解决

--- src/main/java/com/example/SpingBootStudyApplication.java
+++ src/main/java/com/example/SpingBootStudyApplication.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan(basePackages={"com.example"})
+@MapperScan("com.example.dao")//扫描dao,dao层加注解扫描不到，原因待查,service层可以,不要用
+                              //@ComponentScan,会覆盖@SpringBootApplication默认扫描同级包和子包注解
 public class SpingBootStudyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/dao/CustomerDao.java
+++ src/main/java/com/example/dao/CustomerDao.java
@@ -2,11 +2,12 @@ package com.example.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.Customer;
 
-@Repository
+
+//@Mapper 这个注解springboot启动类扫描dao，其他@Component,@Repository都不行，或者在启动类加@MapperScan扫描dao包
 public interface CustomerDao {
      
 	List<Customer> selectList();
