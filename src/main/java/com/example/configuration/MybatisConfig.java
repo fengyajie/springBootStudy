71b712fdfb85b137f412500e4cb50512cd019021
lijc <lijc0208@163.com>
2018-11-16 19:44:07 +0800

    多数据源配置

--- src/main/java/com/example/configuration/MybatisConfig.java
+++ src/main/java/com/example/configuration/MybatisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
@@ -30,6 +31,10 @@ public class MybatisConfig {
 	public SqlSessionFactory shiroSqlSessionFactory(@Qualifier("shiroDataSource") DataSource dataSource) throws Exception{
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.
+        setMapperLocations(new PathMatchingResourcePatternResolver().
+                getResources("classpath*:com/example/mapper/customerShiro/*.xml"));
+
 		return sqlSessionFactoryBean.getObject();
 	}
 	
